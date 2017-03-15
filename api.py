0e60bd20d0fc388319d2e60202ffd3f2fd650685
schneefux <schneefux+commit@schneefux.xyz>
2017-03-15 15:01:57 +0100

    log debug to console only

--- api.py
+++ api.py
@@ -87,17 +87,7 @@ async def startup():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        filename=os.path.realpath(
-            os.path.join(os.getcwd(),
-                         os.path.dirname(__file__))) +
-            "/logs/apigrabber.log",
-        filemode="a",
-        level=logging.DEBUG
-    )
-    console = logging.StreamHandler()
-    console.setLevel(logging.WARNING)
-    logging.getLogger("").addHandler(console)
+    logging.basicConfig(level=logging.DEBUG)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(startup())
